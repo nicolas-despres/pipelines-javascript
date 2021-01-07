c5d318b58abece2118e77205ba7d002d02865915
Nicolas Despres <nicolas.despres@mphasis.com>
2021-01-07 08:54:20 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,7 @@ describe('String', function () {
     describe('#indexOf', function () {
         it('should return -1 when the substring is not present', function () {
             var myString = 'test';
+            console.log(myString)
 
             assert.equal(-1, myString.indexOf('t'));
             assert.equal(-1, myString.indexOf('y'));
